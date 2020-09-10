aa01eb0663b9c8cd972d296674de72a5166d7024
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-10 12:54:24 +0530

    add comments in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,35 @@
 echo "Setting up your MacBook"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" #install homebrew
-brew install htop;
-brew install google-chrome;
-brew install slack;
-brew install git;
-brew install vim;
-brew install node;
-brew install sqlite;
-brew install redis;
-brew install travis;
-brew install jq;
-brew install curl;
-brew install zsh;
+#install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+#install zsh and set it as default shell, also install some plugins
+brew install zsh zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /bin/zsh;
-brew install zsh-autosuggestions;
-brew install bat;
-brew install tree;
-brew tap heroku/brew
-brew install heroku
-brew cask install iterm2
-brew cask install visual-studio-code
-brew cask install google-chat
+
+#install spaceship propmpt
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" 
+
+#install command line tools
+brew install htop git vim node sqlite redis travis jq curl bat tree
+brew tap heroku/brew
+brew install heroku
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
 
-######## Applications need to be manually installed 
+#install applications
+brew install google-chrome slack
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install google-chat
+
+# Applications need to be manually installed 
 # mindnode
 # zoom
-##########
 
-cp .vimrc .zshrc ~; #setting up vim and zsh
+#setting up vim and zsh
+cp .vimrc .zshrc ~;
 
 echo "\n\n----------------\n Done..."
